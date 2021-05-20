501e528b3d59f895a1b37fd750deafdba2607a9f
Marcin Banaszkiewicz <marcin.banaszkiewicz@300brains.com>
2021-05-20 23:15:15 +0200

    fetch API data

--- components/SearchField/index.tsx
+++ components/SearchField/index.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from "react";
 import { StyledTextInput, TextInputWrapper } from "../SearchField/styles";
 
-export const SearchField: FC<any> = () => {
+export const SearchField: FC<any> = ({ value, onChangeText }) => {
   return (
     <TextInputWrapper>
-      <StyledTextInput placeholder="   Github user" />
+      <StyledTextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder="Github user"
+      />
     </TextInputWrapper>
   );
 };
--- screens/search-screen/TabOneScreen.tsx
+++ screens/search-screen/TabOneScreen.tsx
@@ -1,6 +1,6 @@
 import { StackNavigationProp } from "@react-navigation/stack";
 import * as React from "react";
-import { FC, useEffect, useContext } from "react";
+import { FC, useEffect, useState } from "react";
 import { StyledContainer } from "../../styles/styles";
 import { BottomTabParamList } from "../../types";
 import { SearchField } from "../../components/SearchField";
@@ -12,11 +12,29 @@ interface IProps {
   navigation: StackNavigationProp<BottomTabParamList, "SearchScreen">;
 }
 export const TabOneScreen: FC<IProps> = ({ navigation }) => {
-  useEffect(() => {});
+  const [userName, setUserName] = useState("");
+  const [userData, setUserData] = useState({});
+
+  const handleUserName = (text: string) => {
+    setUserName(text);
+  };
+
+  useEffect(() => {
+    const fetchData = setTimeout(() => {
+      fetch(`https://api.github.com/users/${userName}`)
+        .then((response) => response.json())
+        .then((data) => setUserData(data));
+      console.log(userData);
+    }, 500);
+
+    return () => {
+      clearTimeout(fetchData);
+    };
+  }, [userName]);
 
   return (
     <StyledContainer>
-      <SearchField />
+      <SearchField value={userName} onChangeText={handleUserName} />
       <DetailsField backgroundColor={colors.whiteColor} />
       <Button
         color={colors.buttonBackground}
